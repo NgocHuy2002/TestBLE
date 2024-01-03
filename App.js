cebd33e4872147173bf13151e4dd46914ee023ab
NgocHuy2002 <ngochuyk23@gmail.com>
2024-01-03 16:25:09 +0700

    update

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { BleManager } from 'react-native-ble-plx';
+import btlogo from './assets/bluetooth-logo.png';
+
+export const manager = new BleManager();
 
 export default function App() {
+  // const scanAndConnect = () => {
+  //   console.log("scanAndConnect");
+  //   manager.startDeviceScan(null, null, (error, device) => {
+  //     if (error) {
+  //         // Handle error (scanning will be stopped automatically)
+  //         return
+  //     }
+
+  //     console.log("Found",device.name, device.localName);
+
+  //     // Check if it is a device you are looking for based on advertisement data
+  //     // or other criteria.
+  //     // if (device.name === 'TI BLE Sensor Tag' || 
+  //     //     device.name === 'SensorTag') {
+          
+  //     //     // Stop scanning as it's not necessary if you are scanning for one device.
+  //     //     manager.stopDeviceScan();
+
+  //     //     // Proceed with connection.
+  //     // }
+  //   });
+  // }
+
+  // React.useEffect(() => {
+  //   console.log("Setting up initial manager subscription");
+  //   manager.onStateChange((state) => {
+  //     const subscription = manager.onStateChange((state) => {
+  //         if (state === 'PoweredOn') {
+  //             scanAndConnect();
+  //             subscription.remove();
+  //         }
+  //     }, true);
+  //     return () => subscription.remove();
+  //   });
+  // }, [manager]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={btlogo} style={styles.logoImage}/>
+      <Text>React Native Bluetooth Example</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +59,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logoImage: {
+    width: 100,
+    height: 132,
+    resizeMode: 'stretch'
+  }
 });
